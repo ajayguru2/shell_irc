8bff51a18e5886fecf2df2721dde7c3af0532397
Ajay Guru <201651005@iiitvadodara.ac.in>
2017-10-16 00:13:11 +0530

    help added

--- index.js
+++ index.js
@@ -7,34 +7,40 @@ var time = date.getTime();
 
 
 // console.log(argv);
-var tag = argv.t;
-// console.log(tag);
+if (argv.t) {
+    var tag = argv.t;
+    console.log(tag);
 
-var address = `https://webchat.freenode.net/?channels=${tag}`;
-console.log(address);
+    var address = `https://webchat.freenode.net/?channels=${tag}`;
+    console.log(address);
 
-net.open(address);
-
-
-var logObject = {
-    'tag': tag,
-    'address': address,
-    'time': time
-};
+    net.open(address);
 
 
-
+    var logObject = {
+        'tag': tag,
+        'address': address,
+        'time': time
+    };
 
 
 // console.log(logObject);
-var str = JSON.stringify(logObject);
+    var str = JSON.stringify(logObject);
 
-fs.appendFile('history', `${str}\n` , (err) => {
-    if (err) throw err;
-    console.log('the channel is up!!');
-});
+    fs.appendFile('history', `${str}\n`, (err) => {
+        if (err) throw err;
+        console.log('the channel is up!!');
+    });
 
 // /prompt();
+}
+
+if (argv.h){
+    console.log("\n\n\nshell_irc is a relay connector commad line tool\n\n===============================\n\nsample:\nnode index.js -t [tag-name]\n\n===============================\n\n\n");
+
+}
+
+
 
 
 
